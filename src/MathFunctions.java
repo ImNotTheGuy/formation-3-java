b564765b53610209932a43cc62e42b409392db79
Ludo <lraulin843@gmail.com>
2022-06-09 11:41:21 +0200

    added constants

---
+++ src/MathFunctions.java
@@ -0,0 +1,16 @@
+public class MathFunctions {
+    public static void main(String[] args) {
+
+        double number1 = 81;
+        double number2 = 25;
+
+        // Calculate square root of 81
+
+        Math.sqrt(number1);
+
+        // Calculate 25 to the power of 3
+
+        Math.pow(25, 3);
+
+    }
+}
---
+++ src/Read.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Read {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Name: "); // note that we use print and not println to avoid writing on the line below
+        String name = input.nextLine();
+
+
+        System.out.print("Gender: (M/F or N) ");
+        char gender = input.next().charAt(0);
+
+        System.out.print("Your age: ");
+        int age = input.nextInt();
+
+        System.out.print("Your weight: ");
+        float weight = input.nextFloat();
+
+        System.out.println("Name: " + name );
+        System.out.println("Gender: " + gender );
+        System.out.println("Age: " + age );
+        System.out.println("Weight: " + weight );
+
+
+    }
+
+}
